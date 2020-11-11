91f2f3d948fdaf2260abdc19bf9bcc7dc7d6c62c
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-11 23:48:04 +0100

    aha

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -6,7 +6,25 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    a = a + 1;
+                }
+            }
+            int[] vysledok = new int[a];
+            for (int ii = 0; ii < numbers.Length; ii++)
+            {
+                if (numbers[ii] % 2 == 0)
+                {
+                    vysledok[b] = numbers[ii];
+                    b = b + 1;
+                }
+            }
+            return vysledok;
         }
     }
 }
